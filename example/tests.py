94bab402d565bbc60e52c068df358fee8d42bd82
Dave Koopman <dkoopman@godaddy.com>
2015-01-11 19:27:00 -0700

    Switch to all self.example

--- example/tests.py
+++ example/tests.py
@@ -12,30 +12,26 @@ class ExampleTestCase(TestCase):
 
     @patch('example.models.request')
     def test_get_page(self, req):
-        example = Example.objects.get(name='Example 1')
         url = MagicMock()
-        example._get_page(url)
+        self.example._get_page(url)
         req.assert_called_once_with('GET', url)
 
     @patch('example.models.Example._get_page')
     def test_name_in_page_calls_get_page(self, getpage):
-        example = Example.objects.get(name='Example 1')
         url = MagicMock()
-        example.name_in_page(url)
+        self.example.name_in_page(url)
         getpage.assert_called_once_with(url)
 
     @patch('example.models.Example._get_page')
     def test_name_in_page(self, getpage):
-        example = Example.objects.get(name='Example 1')
         resp = MagicMock()
-        resp.content = 'Text that contains {} in it'.format(example.name)
+        resp.content = 'Text that contains {} in it'.format(self.example.name)
         getpage.return_value = resp
-        self.assertTrue(example.name_in_page(MagicMock()))
+        self.assertTrue(self.example.name_in_page(MagicMock()))
 
     @patch('example.models.Example._get_page')
     def test_name_in_page_not(self, getpage):
-        example = Example.objects.get(name='Example 1')
         resp = MagicMock()
         resp.content = 'Text that does not contain the name'
         getpage.return_value = resp
-        self.assertFalse(example.name_in_page(MagicMock()))
+        self.assertFalse(self.example.name_in_page(MagicMock()))
